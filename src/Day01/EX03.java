997fc1e82c382243495ed3f9716a900942d294da
hyungdoyou <yhd4286@gmail.com>
2023-11-20 16:14:49 +0900

    [Docs] EX03 수정
    본문 수정

--- src/Day01/EX03.java
+++ src/Day01/EX03.java
@@ -1,5 +1,7 @@
 package Day01;
 
+import java.sql.SQLOutput;
+
 public class EX03 {
     public static void main(String[] args) {
         // 1. 다음과 같이 정사각형을 출력하는 코드 작성
@@ -33,8 +35,9 @@ public class EX03 {
             System.out.println();
         }
         System.out.println("------------");
+
         // 3. 다음과 같이 삼각형을 출력하는 코드 작성
-        /*
+        /* 첫번째 반복문에서는 공백을 점점 줄어들게(4, 3, 2, 1, 0) 출력 / *을 2씩 커지면서 반복횟수만큼 반복해서 출력
         ----------
              *
             ***
@@ -43,15 +46,15 @@ public class EX03 {
          **********
          ----------
          */
-        for (Integer i = 1; i < 6; i++) {
-            for (Integer j = 0; j < i; j++) {
-                for(Integer y=0; y<j; y++) {
-                    System.out.print(" ");
-                }
+        for(Integer i=1; i<6; i++) {
+            for (Integer j=6; j > i+1; j--) {
+                System.out.print(" ");
+            }
+            for (Integer k=0; k<2*i-1; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        System.out.println("------------");
+        System.out.println("------------------------");
     }
 }
